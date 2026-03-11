1f86e6b91c9119ddc08b5f36289180b1556c61a0
Yzm <yanziming022@gmail.com>
2026-03-11 13:17:03 +0800

    chore(project): 移除项目中的环境配置文件和相关组件

---
+++ Image-Annotation-frontend/src/components/Image-Annotation/Git/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script setup>
+const url = ref('REPLACE_WITH_YOUR_REPO_URL');
+
+function goto() {
+  window.open(url.value)
+}
+</script>
